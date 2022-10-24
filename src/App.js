101db0dfa036db8f4b2db837601841a7a44b41e4
Kitan Garcia <kitangar@gmail.com>
2022-10-24 14:12:04 -0700

    add rendering of gifs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import "./App.css";
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+  "https://imgs.search.brave.com/hZuSHh7HRUdx1xCOEV0rdNE4nmuyHsXRRA74hxPOH4w/rs:fit:350:308:1/g:ce/aHR0cHM6Ly82OC5t/ZWRpYS50dW1ibHIu/Y29tL2RhMzkwMjhm/NzE0MTRjZjgwOGQ2/ODRiMDE4OTExM2M0/L3R1bWJscl9uZHRh/ZHZCWnIzMXRlNXJ1/c28xXzQwMC5naWY.gif",
+  "https://imgs.search.brave.com/bprsaq_OCGub754wsIaoqnW1TiMgSbSi5nHj4-qNvy4/rs:fit:499:260:1/g:ce/aHR0cHM6Ly82Ni5t/ZWRpYS50dW1ibHIu/Y29tLzRmYmFjMzg5/OWVkMDUwMTQ2MWI4/MzIwMWY5ZTJkYThl/L3R1bWJscl9tamNy/Y2pRTlU5MXJ3eTIx/cG8xXzUwMC5naWY.gif",
+  "https://media0.giphy.com/media/m171D9eM7Jq6s/200.gif",
+  "https://media1.giphy.com/media/P9KgGibY0sKwf8Q3WI/200.gif",
+];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -42,6 +48,24 @@ const App = () => {
     </button>
   );
 
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input type="text" placeholder="Enter GIF link" />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
+        </button>
+      </form>
+      <div className="gif-grid">
+        {TEST_GIFS.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
@@ -54,11 +78,12 @@ const App = () => {
     <div className="App">
       <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼Animal GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
